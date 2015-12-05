a8b475d449f67bb40e7ede9da239c4f45b97d0c5
Edmund Mai <edmundm@crowdtap.com>
2015-12-05 11:54:03 -0500

    adds text to enlarged view and installs iads

--- travelbuddy/ViewController.swift
+++ travelbuddy/ViewController.swift
@@ -7,23 +7,25 @@
 //
 
 import UIKit
+import iAd
 
 class ViewController: UIViewController {
 
   let iconMapper = [
-    0: "toilet-1",
-    1: "atm-machine",
-    2: "food",
-    3: "charger-1",
-    4: "subway",
-    5: "cafe",
-    6: "airport",
-    7: "taxi",
-    8: "wifi"
+    0: "Toilet",
+    1: "ATM",
+    2: "Food",
+    3: "Electric Outlet",
+    4: "Subway",
+    5: "Coffee",
+    6: "Airport",
+    7: "Taxi",
+    8: "Wifi"
   ]
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.canDisplayBannerAds = true
     // Do any additional setup after loading the view, typically from a nib.
   }
 
@@ -75,12 +77,19 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     let enlargedView = UIView(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
     enlargedView.backgroundColor = UIColor(red: 0, green: 111, blue: 234, alpha: 1.0)
     
-    
     let enlargedImageView = UIImageView(frame: CGRectMake(UIScreen.mainScreen().bounds.width*0.1, UIScreen.mainScreen().bounds.height/4, UIScreen.mainScreen().bounds.width*0.8, UIScreen.mainScreen().bounds.height/2))
     enlargedImageView.image = cell.imageView.image
     
     enlargedView.addSubview(enlargedImageView)
     
+    let translationTextLabel = UILabel(frame: CGRectMake(UIScreen.mainScreen().bounds.width*0.1, UIScreen.mainScreen().bounds.height * 4/5, UIScreen.mainScreen().bounds.width*0.8, 30))
+    translationTextLabel.text = iconMapper[indexPath.row]!
+    translationTextLabel.font = UIFont.systemFontOfSize(25)
+    
+
+    translationTextLabel.textAlignment = NSTextAlignment.Center
+    
+    enlargedView.addSubview(translationTextLabel)
 
     let tap = UITapGestureRecognizer(target: self, action: Selector("largeImageTapped:"))
     enlargedView.addGestureRecognizer(tap)
@@ -90,19 +99,5 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     UIView.beginAnimations(nil, context: nil)
     enlargedView.alpha = 1.0
     UIView.commitAnimations()
-//
-//    let button = UIButton(frame: CGRectMake(0, 0, UIScreen.mainScreen().bounds.width, UIScreen.mainScreen().bounds.height))
-//    //    button.setTitle("heyyyy", forState: .Normal)
-//    button.backgroundColor = UIColor(red: 0, green: 111, blue: 234, alpha: 1.0)
-//    button.setImage(cell.imageView.image, forState: .Normal)
-//    
-//    let tap = UITapGestureRecognizer(target: self, action: Selector("largeImageTapped:"))
-//    button.addGestureRecognizer(tap)
-//    
-//    button.alpha = 0.0
-//    self.view.addSubview(button)
-//    UIView.beginAnimations(nil, context: nil)
-//    button.alpha = 1.0
-//    UIView.commitAnimations()
   }
 }
